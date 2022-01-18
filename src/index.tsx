7785406e0ac67d03e6dd36fa85882adbb24ca001
Dada Khalandar <kerry.blig12@gmail.com>
2022-01-18 23:40:49 +0530

    imported and added chakra ui to the index file

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { ChakraProvider } from '@chakra-ui/react';
+
 import App from './App';
-import { BrowserRouter } from 'react-router-dom'
 
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
