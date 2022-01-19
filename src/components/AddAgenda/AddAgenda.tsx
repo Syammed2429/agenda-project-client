82e8934da165c411b9fb2cd24f0a038754b15e5d
Dada Khalandar <kerry.blig12@gmail.com>
2022-01-19 12:09:28 +0530

    added a form to add agendas

--- src/components/AddAgenda/AddAgenda.tsx
+++ src/components/AddAgenda/AddAgenda.tsx
@@ -7,6 +7,11 @@ import {
   FormHelperText,
   Button,
   Input,
+  Text,
+  Checkbox,
+  Stack,
+  Radio,
+  RadioGroup,
 } from "@chakra-ui/react";
 
 const AddAgenda: FC = () => {
@@ -25,7 +30,7 @@ const AddAgenda: FC = () => {
             <Input
               id="title"
               type="text"
-              value={input}
+              //   value={input}
               onChange={handleInputChange}
             />
             {!isError ? (
@@ -33,7 +38,51 @@ const AddAgenda: FC = () => {
             ) : (
               <FormErrorMessage>Title is required.</FormErrorMessage>
             )}
-            <Button type="submit">Submit</Button>
+            <FormLabel htmlFor="description">Description</FormLabel>
+            <Input
+              id="description"
+              type="text"
+              //   value={input}
+              onChange={handleInputChange}
+            />
+            {!isError ? (
+              <FormHelperText>Enter the title of agenda item.</FormHelperText>
+            ) : (
+              <FormErrorMessage>Title is required.</FormErrorMessage>
+            )}
+            <FormLabel htmlFor="status">Status</FormLabel>
+            <Text>Is it completed?</Text>
+
+            <RadioGroup defaultValue="2">
+              <Stack spacing={5} direction="row">
+                <Radio colorScheme="red" value="1">
+                  Yes
+                </Radio>
+                <Radio colorScheme="green" value="2">
+                  No
+                </Radio>
+              </Stack>
+            </RadioGroup>
+            {!isError ? (
+              <FormHelperText>Enter the title of agenda item.</FormHelperText>
+            ) : (
+              <FormErrorMessage>Title is required.</FormErrorMessage>
+            )}
+            <FormLabel htmlFor="date">date</FormLabel>
+            <Input
+              id="date"
+              type="date"
+              value={input}
+              onChange={handleInputChange}
+            />
+            {!isError ? (
+              <FormHelperText>Enter the date of agenda item.</FormHelperText>
+            ) : (
+              <FormErrorMessage>Title is required.</FormErrorMessage>
+            )}
+            <Button my={3} type="submit">
+              Add Agenda
+            </Button>
           </FormControl>
         </form>
       </Container>
