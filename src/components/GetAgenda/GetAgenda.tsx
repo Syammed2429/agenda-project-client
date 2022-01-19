fc384224d02eb0701bed96d1ca79a85f9e3492cf
Dada Khalandar <kerry.blig12@gmail.com>
2022-01-19 15:01:22 +0530

    added the fetched data to the usestate hook

--- src/components/GetAgenda/GetAgenda.tsx
+++ src/components/GetAgenda/GetAgenda.tsx
@@ -1,21 +1,37 @@
-import React, { FC, useEffect } from "react";
+import React, { FC, useEffect, useState } from "react";
 import axios from "axios";
+import { Container } from "@chakra-ui/react";
 
 const GetAgenda: FC = () => {
+  //Hooks
+  const [agendaItems, setAgendaItems] = useState<
+    | {
+        _id: string | number;
+        date: string | number | Date;
+        description: string;
+        statue: boolean;
+        title: string;
+      }[]
+    | null
+  >(null);
+
   //Backend link
   const BELink: string | undefined = process.env.REACT_APP_BACKEND_URL;
 
   useEffect(() => {
     //Fetching all the agenda items from the backend
     const getAllAgendaItems = async () => {
-      const response = await fetch(`${BELink}/agenda`);
-      const result = await response.json();
-      console.log("result:", result);
+      const { data } = await axios.get(`${BELink}/agenda`);
+      setAgendaItems(data.agendas);
     };
     getAllAgendaItems();
   }, [BELink]);
 
-  return <></>;
+  return (
+    <>
+      <Container></Container>
+    </>
+  );
 };
 
 export { GetAgenda };
