9ea366b4cb090485bce1b5bbdd86b2964566d091
Dada Khalandar <kerry.blig12@gmail.com>
2022-01-19 11:05:32 +0530

    added a icons for the color changing mode

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from "react";
+import { BsMoonFill, BsSunFill } from "react-icons/bs";
 import {
   Button,
   Flex,
@@ -9,23 +10,24 @@ import {
 } from "@chakra-ui/react";
 
 import logo from "../../assets/logo.png";
+
 const Navbar: FC = () => {
   const { colorMode, toggleColorMode } = useColorMode();
 
   return (
     <>
-      <Flex alignItems="center">
+      <Flex boxShadow="dark-lg" p="3" alignItems="center">
         {/* Logo */}
         <Image w={120} src={logo} alt="Agenda logo" />
 
         {/* Navbar Items start */}
-        <HStack position="absolute" right="0" spacing="24px">
+        <HStack position="absolute" right="4" spacing="24px" fontSize="18">
           <Text>Add Agenda</Text>
           <Text>Check Agenda Items & export</Text>
           <Text>Import CSV</Text>
 
           <Button onClick={toggleColorMode}>
-            Toggle {colorMode === "light" ? "Dark" : "Light"}
+            {colorMode === "light" ? <BsMoonFill /> : <BsSunFill />}
           </Button>
         </HStack>
         {/* Navbar Items End */}
