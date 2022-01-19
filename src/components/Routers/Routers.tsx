82d57231f27a3750e991b38b0cdb0cacaf88427b
Dada Khalandar <kerry.blig12@gmail.com>
2022-01-19 13:57:26 +0530

    added routes to the homepage

--- src/components/Routers/Routers.tsx
+++ src/components/Routers/Routers.tsx
@@ -1,7 +1,17 @@
 import React, { FC } from "react";
+import { Routes, Route } from "react-router-dom";
 
+import { AddAgenda } from "../AddAgenda/AddAgenda";
+import { Navbar } from "../Navbar/Navbar";
 const Routers: FC = () => {
-  return <></>;
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="" element={<AddAgenda />} />
+      </Routes>
+    </>
+  );
 };
 
 export { Routers };
