df204c9048bcb044110ac2e57674adac90b28e09
Dada Khalandar <kerry.blig12@gmail.com>
2022-01-19 10:36:32 +0530

    added themeconfig

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, extendTheme, ThemeConfig, ColorModeScript } from '@chakra-ui/react'
+
 
 import App from './App';
+
+
+const config: ThemeConfig = {
+  initialColorMode: 'system',
+  useSystemColorMode: true,
+}
+
+// 3. extend the theme
+const theme = extendTheme({ config })
 
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <ChakraProvider>
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
         <App />
       </ChakraProvider>
     </BrowserRouter>
