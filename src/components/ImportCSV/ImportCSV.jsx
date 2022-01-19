9ec2443918964d3a9beb3efd102a3c99c036d904
Dada Khalandar <kerry.blig12@gmail.com>
2022-01-19 18:55:37 +0530

    added a function to process the csv data

--- src/components/ImportCSV/ImportCSV.jsx
+++ src/components/ImportCSV/ImportCSV.jsx
@@ -3,20 +3,44 @@ import * as XLSX from "xlsx";
 import DataTable from "react-data-table-component";
 import { Text } from "@chakra-ui/react";
 
-const ImportCSV: FC = () => {
+const ImportCSV = () => {
   //Hooks
-  const [columns, setColumns] = useState<string[] | null>(null);
-  const [data, setData] = useState<string[] | null>(null);
+  const [columns, setColumns] = useState([]);
+  const [data, setData] = useState([]);
 
   // process CSV data
-  const processData = (dataString: any) => {
+  const processData = (dataString) => {
     const dataStringLines = dataString.split(/\r\n|\n/);
     const headers = dataStringLines[0].split(
       /,(?![^"]*"(?:(?:[^"]*"){2})*[^"]*$)/
     );
 
-    const list: string[] | undefined = [];
-  };
+    const list = [];
+    for (let i = 1; i < dataStringLines.length; i++) {
+      const row = dataStringLines[i].split(
+        /,(?![^"]*"(?:(?:[^"]*"){2})*[^"]*$)/
+      );
+      if (headers && row.length == headers.length) {
+        const obj = {};
+        for (let j = 0; j < headers.length; j++) {
+          let d = row[j];
+          if (d.length > 0) {
+            if (d[0] == '"') d = d.substring(1, d.length - 1);
+            if (d[d.length - 1] == '"') d = d.substring(d.length - 2, 1);
+          }
+          if (headers[j]) {
+            obj[headers[j]] = d;
+          }
+        }
+
+        // remove the blank rows
+        if (Object.values(obj).filter((x) => x).length > 0) {
+          list.push(obj);
+        }
+      }
+
+    }
+  }
 
   return (
     <>
