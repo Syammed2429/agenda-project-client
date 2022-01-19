3b9a39ab1d663524a3025d444191b6faaed1966e
Dada Khalandar <kerry.blig12@gmail.com>
2022-01-19 13:58:40 +0530

    added a route to get the agenda items

--- src/components/Routers/Routers.tsx
+++ src/components/Routers/Routers.tsx
@@ -2,6 +2,7 @@ import React, { FC } from "react";
 import { Routes, Route } from "react-router-dom";
 
 import { AddAgenda } from "../AddAgenda/AddAgenda";
+import { GetAgenda } from "../GetAgenda/GetAgenda";
 import { Navbar } from "../Navbar/Navbar";
 const Routers: FC = () => {
   return (
@@ -9,6 +10,7 @@ const Routers: FC = () => {
       <Navbar />
       <Routes>
         <Route path="" element={<AddAgenda />} />
+        <Route path="/get-agenda" element={<GetAgenda />} />
       </Routes>
     </>
   );
