f1de4d1289c79de4a982491f27ee1ad29b8d20ba
Dada Khalandar <kerry.blig12@gmail.com>
2022-01-19 14:02:31 +0530

    added a route for importing csv

--- src/components/Routers/Routers.tsx
+++ src/components/Routers/Routers.tsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 
 import { AddAgenda } from "../AddAgenda/AddAgenda";
 import { GetAgenda } from "../GetAgenda/GetAgenda";
+import { ImportCSV } from "../ImportCSV/ImportCSV";
 import { Navbar } from "../Navbar/Navbar";
 const Routers: FC = () => {
   return (
@@ -11,6 +12,7 @@ const Routers: FC = () => {
       <Routes>
         <Route path="" element={<AddAgenda />} />
         <Route path="/get-agenda" element={<GetAgenda />} />
+        <Route path="/import-csv" element={<ImportCSV />} />
       </Routes>
     </>
   );
