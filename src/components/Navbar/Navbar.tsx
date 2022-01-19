2ab57e27db7dff7b04c6429e1c3a085a512d8ae3
Dada Khalandar <kerry.blig12@gmail.com>
2022-01-19 10:59:40 +0530

    added navbar items

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,29 +1,37 @@
-import React, { FC } from 'react'
-import { Button, Flex, Image, useColorMode } from '@chakra-ui/react'
+import React, { FC } from "react";
+import {
+  Button,
+  Flex,
+  HStack,
+  Image,
+  Text,
+  useColorMode,
+} from "@chakra-ui/react";
 
-import logo from '../../assets/logo.png'
+import logo from "../../assets/logo.png";
 const Navbar: FC = () => {
-    const { colorMode, toggleColorMode } = useColorMode()
+  const { colorMode, toggleColorMode } = useColorMode();
 
+  return (
+    <>
+      <Flex alignItems="center">
+        {/* Logo */}
+        <Image w={120} src={logo} alt="Agenda logo" />
 
-    return (
-        <>
-            <Flex
-                alignItems='center'
+        {/* Navbar Items start */}
+        <HStack position="absolute" right="0" spacing="24px">
+          <Text>Add Agenda</Text>
+          <Text>Check Agenda Items & export</Text>
+          <Text>Import CSV</Text>
 
-            >
+          <Button onClick={toggleColorMode}>
+            Toggle {colorMode === "light" ? "Dark" : "Light"}
+          </Button>
+        </HStack>
+        {/* Navbar Items End */}
+      </Flex>
+    </>
+  );
+};
 
-                <Image
-                    w={120}
-                    src={logo}
-                    alt='Agenda logo' />
-
-                <Button onClick={toggleColorMode}>
-                    Toggle {colorMode === 'light' ? 'Dark' : 'Light'}
-                </Button>
-            </Flex>
-        </>
-    )
-}
-
-export { Navbar }
+export { Navbar };
