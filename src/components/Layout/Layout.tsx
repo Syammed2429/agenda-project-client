c448a067a9adb787d8aca5a2a3e40f99963d1f5c
Dada Khalandar <kerry.blig12@gmail.com>
2022-01-19 10:35:18 +0530

    created a layout where we can add all the components at one place

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,15 @@
+import { Box } from '@chakra-ui/react'
+import React, { FC } from 'react'
+import { Navbar } from '../Navbar/Navbar'
+
+const Layout: FC = () => {
+    return (
+        <>
+            <Box>
+                <Navbar />
+            </Box>
+        </>
+    )
+}
+
+export { Layout }
